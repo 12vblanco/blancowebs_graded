fdd7f1af7874aa41c77ea30643ee4f01b24714b6
Victor Blanco <52953604+BlancoWebs@users.noreply.github.com>
2021-03-31 01:27:07 +0100

    1

--- js/open_form.js
+++ js/open_form.js
@@ -6,14 +6,15 @@ button.addEventListener('click', () => {
     form.style.height = '50rem';
     form.style.visibility = 'visible';
     form.style.opacity = '1';
-    document.getElementById("form").style.transition = "all .4s";
+    document.getElementById("form").style.transition = "height .4s, opacity .5s";
 
 });
 
 close.addEventListener('click', () => {
+    
     form.style.height = '0';
     form.style.visibility = 'hidden';
-    document.getElementById("form").style.transition = "height 0.4s";
-    // document.getElementById("form").style.transition = "opacity 0.2s";
+    form.style.opacity = '0';
 
+    document.getElementById("form").style.transition = "height 0.4s, opacity .4s";
 });
