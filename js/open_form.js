7cd68225cd1a006471c7e788f4a481acd4c921f2
Victor Blanco <52953604+BlancoWebs@users.noreply.github.com>
2021-04-02 13:49:09 +0100

    1

--- js/open_form.js
+++ js/open_form.js
@@ -21,6 +21,19 @@ close.addEventListener('click', () => {
 
 
 // WORKS
+
+// Copyright (c) 2021 by Frederic R. (https://codepen.io/fredericrous/pen/xxVXJYX)
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// https://codepen.io/fredericrous/pen/xxVXJYX
+
+const isApple = /apple/i.test(navigator.vendor);
+isApple ? "proximity" : "unset";
 const slider = document.querySelector('.wrapper');
 let isDown = false;
 let startX;
@@ -48,3 +61,4 @@ slider.addEventListener('mousemove', e => {
   const walk = (x - startX) * SCROLL_SPEED;
   slider.scrollLeft = scrollLeft - walk;
 });
+
