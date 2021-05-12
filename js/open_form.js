0a76a1b20b9883d5af0bc7fec58cae9f62298c3a
Victor Blanco <52953604+12vblanco@users.noreply.github.com>
2021-05-12 18:57:03 +0100

    contact

--- js/open_form.js
+++ js/open_form.js
@@ -1,20 +1,20 @@
 /* FORM*/
 const button = document.querySelector('.button_open');
-const form = document.querySelector('#form');
+const form = document.querySelector('#main_form');
 const close = document.querySelector('.close');
 // Open form
 button.addEventListener('click', () => {
   form.style.height = '50rem';
   form.style.visibility = 'visible';
   form.style.opacity = '1';
-  document.getElementById('form').style.transition = 'height .4s, opacity .5s';
+  document.getElementById('main_form').style.transition = 'height .4s, opacity .5s';
 });
 // Close form
 close.addEventListener('click', () => {
   form.style.height = '0';
   form.style.visibility = 'hidden';
   form.style.opacity = '0';
-  document.getElementById('form').style.transition = 'height 0.4s, opacity .4s';
+  document.getElementById('main_form').style.transition = 'height 0.4s, opacity .4s';
 });
 //  CONTACT BTN
 const contact = document.querySelectorAll('.open_contact');
